905dba2857f19f9f6b86689f6e16dab498fce5e3
Beth-Hollinger <hollingere15@gmail.com>
2022-09-01 21:11:45 -0400

    update and add functionality to all pages

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const pokemon = require('./models/pokemon.js');
 const app = express();
 const port = 3000
 const Pokemon = require('./models/pokemon.js');
@@ -8,27 +9,53 @@ app.use(express.urlencoded({extended: false}));
 // INDEX
 
 app.get('/pokemon', (req, res) => {
-res.render('index.ejs', { data: Pokemon });
+res.render('index.ejs', { 
+        data: pokemon 
+    });
+});
+
+//NEW
+app.get("/pokemon/new", (req, res) => {
+    res.render("new.ejs")
+})
+
+//DELETE
+app.delete("/pokemon/:id", (req, res) => {
+    data.splice(req.params.id, 1) /
+    res.redirect("/pokemon") 
+  })
+
+//UPDATE
+app.put("/pokemon/:id", (req, res) => {
+  pokedex[req.params.id] = req.body
+  res.redirect("/pokemon")
+})
+
+//CREATE
+app.post("/pokemon/:id", (req, res) => {
+    pokemon.push[req.body]
+    res.redirect("/pokemon")
+})
+
+
+//EDIT
+app.get("/pokemon/:id/edit", (req, res) => {
+    res.render(
+      "edit.ejs", 
+      {
+        pokemon: data[req.params.id], 
+        id: req.params.indexOfFruitsArray, 
+      }
+    )
+  })
+
+//SHOW
+app.get('/pokemon/:id', (req, res) => {
+res.render('show.ejs', { data: pokemon[req.params.id] });
 });
 
 
-
-// // SHOW
-// app.get('photos/:id', (req, res) => {
-// res.render('show.ejs', { data: Pokemon[req.params.id] });
-// });
-
-//NEW
-
-//EDIT
-
-//CREATE
-//UPDATE
-//DESTROY
-
-
-
 //LISTEN
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Caught One`, port)
 });
